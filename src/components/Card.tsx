875210a81e0eeb2cec980b8a8e61aba25f2a19fb
Leonardo Bertoletti <Leonardo.Bertoletti@edu.pucrs.br>
2024-12-18 01:42:15 -0300

    Ajustando proporções

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,8 +4,8 @@ import ted2 from '../assets/imagens/Passione/ted2.jpeg';
 function Card() {
 return(
     <div className="card">
-        <p>teste</p>
         <img src={ted2} alt='erro'></img>
+        <p>teste</p>
     </div>
 )
 }
