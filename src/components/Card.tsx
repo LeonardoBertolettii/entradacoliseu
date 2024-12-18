dbf1f494ded55f60ae468c23f1146bee9035f5d0
Leonardo Bertoletti <Leonardo.Bertoletti@edu.pucrs.br>
2024-12-17 23:03:09 -0300

    continuação do componente card e teste de fotos

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,11 @@
 import './Card.css'
+import ted2 from '../assets/imagens/Passione/ted2.jpeg';
 
 function Card() {
 return(
     <div className="card">
-        <p>pau grande</p>
+        <p>teste</p>
+        <img src={ted2} alt='erro'></img>
     </div>
 )
 }
